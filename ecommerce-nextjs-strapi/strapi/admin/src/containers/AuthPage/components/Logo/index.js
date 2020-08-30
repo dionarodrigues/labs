0ffb5985b4a4701c60e73a41f2959c282e45ff0f
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-08-30 20:04:04 -0300

    Customize Strapi Admin interface

---
+++ ecommerce-nextjs-strapi/strapi/admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoWonDark from '../../../../assets/images/logo-won-dark.svg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoWonDark} alt="won-logo" />;
+
+export default Logo;
