fb062c1f5862e5a9ef4f45a882eb15247b6dbfaa
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-09-04 08:39:27 -0300

    Create a service to populate games content type

--- ecommerce-nextjs-strapi/strapi/api/game/services/game.js
+++ ecommerce-nextjs-strapi/strapi/api/game/services/game.js
@@ -23,7 +23,7 @@ async function getGameInfo(slug) {
         .getAttribute("xlink:href")
         .replace(/_/g, "")
         .replace(/[^\w-]+/g, "")
-      : "Free",
+      : "FREE",
     short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML,
   };
@@ -74,6 +74,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = 'https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity';
@@ -81,7 +115,8 @@ module.exports = {
     const { data: { products } } = await axios.get(gogApiUrl);
     console.log(products[0]);
 
-    await createManyToManyData([products[2], products[3]])
+    await createManyToManyData([products[2], products[3]]);
+    await createGames([products[2], products[3]]);
 
     // await createName(products[0].publisher, 'publisher')
     // await createName(products[0].developer, 'developer')
