7315521ae6e3c5251e19e11306637712102b2f55
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-09-07 11:42:43 -0300

    add theme

--- ecommerce-nextjs-strapi/client/src/styles/global.ts
+++ ecommerce-nextjs-strapi/client/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -9,7 +9,6 @@ const GlobalStyles = createGlobalStyle`
     src: local('Poppins Light'), local('Poppins-Light'),
         url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -18,7 +17,6 @@ const GlobalStyles = createGlobalStyle`
     src: local('Poppins Regular'), local('Poppins-Regular'),
         url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -27,7 +25,6 @@ const GlobalStyles = createGlobalStyle`
     src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
         url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
   }
-
   * {
     margin: 0;
     padding: 0;
@@ -35,18 +32,15 @@ const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-
-  html {
-    font-size: 62.5%;
-  }
-
-  html, body, #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 `
 
 export default GlobalStyles
