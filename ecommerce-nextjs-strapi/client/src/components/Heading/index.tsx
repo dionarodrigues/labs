26e89612e7b24f6f6e9d58199d11efb16eb4142c
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-09-20 19:42:57 -0300

    improve Heading component

--- ecommerce-nextjs-strapi/client/src/components/Heading/index.tsx
+++ ecommerce-nextjs-strapi/client/src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
+  lineColor?: LineColors
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 )
