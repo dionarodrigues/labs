c3e88ec06e87afbf48a22b778ffba4f4f2efcb04
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-10-03 16:22:12 -0300

    Add Container component and improve Global Styles

--- ecommerce-nextjs-strapi/client/src/styles/global.ts
+++ ecommerce-nextjs-strapi/client/src/styles/global.ts
@@ -1,6 +1,13 @@
 import { createGlobalStyle, css } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -31,14 +38,22 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
-  ${({ theme }) => css`
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `};
     }
   `}
 `
