609db68effe4cbc7cd95c940b05ceddbeab5b2e8
diogorodrigues <diogorodrigues.dev@gmail.com>
2021-02-23 21:25:17 +0000

    Add GameItem component

---
+++ ecommerce-nextjs-strapi/client/src/components/GameItem/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameItem, { GameItemProps } from '.'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00'
+  }
+} as Meta
+
+export const WithPayment: Story<GameItemProps> = (args) => (
+  <GameItem {...args} />
+)
+
+WithPayment.args = {
+  downloadLink: 'https://wongames.com/game/download/21312ndasd'
+}
---
+++ ecommerce-nextjs-strapi/client/src/components/GameItem/test.tsx
@@ -0,0 +1,37 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameItem from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00'
+}
+
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
+  })
+
+  it('should render the item with download link', () => {
+    const downloadLink = 'https://link'
+
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
+})
