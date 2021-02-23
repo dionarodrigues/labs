c447da5f6dbb06ed756f3ff33f1999d77e74f5fe
diogorodrigues <diogorodrigues.dev@gmail.com>
2021-02-23 21:49:08 +0000

    Improve PaymentOptions  component

--- ecommerce-nextjs-strapi/client/src/components/Button/styles.ts
+++ ecommerce-nextjs-strapi/client/src/components/Button/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { darken } from 'polished'
+
 import { ButtonProps } from '.'
 
 export type WrapperProps = {
@@ -24,25 +25,33 @@ const wrapperModifiers = {
   fullWidth: () => css`
     width: 100%;
   `,
+  withIcon: (theme: DefaultTheme) => css`
+    svg {
+      width: 1.5rem;
+
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
+  `,
   minimal: (theme: DefaultTheme) => css`
     background: none;
     color: ${theme.colors.primary};
+
     &:hover {
       color: ${darken(0.1, theme.colors.primary)};
     }
   `,
-  withIcon: (theme: DefaultTheme) => css`
-    svg {
-      width: 1.5rem;
-      & + span {
-        margin-left: ${theme.spacings.xxsmall};
-      }
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
     }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, minimal, hasIcon }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -54,15 +63,17 @@ export const Wrapper = styled.button<WrapperProps>`
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
     text-decoration: none;
+
     &:hover {
       background: ${minimal
         ? 'none'
-        : 'linear-gradient(180deg, #e35565 0%, #d958a6 50%)'};
+        : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
 
     ${!!size && wrapperModifiers[size](theme)};
     ${!!fullWidth && wrapperModifiers.fullWidth()};
     ${!!hasIcon && wrapperModifiers.withIcon(theme)};
     ${!!minimal && wrapperModifiers.minimal(theme)};
+    ${disabled && wrapperModifiers.disabled()};
   `}
 `
--- ecommerce-nextjs-strapi/client/src/components/PaymentOptions/test.tsx
+++ ecommerce-nextjs-strapi/client/src/components/PaymentOptions/test.tsx
@@ -1,9 +1,9 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import cards from './mock'
-
 import PaymentOptions from '.'
+import cards from './mock'
 
 describe('<PaymentOptions />', () => {
   it('should render the saved card options and the add new card button', () => {
@@ -12,5 +12,38 @@ describe('<PaymentOptions />', () => {
     expect(screen.getByLabelText(/4325/)).toBeInTheDocument()
     expect(screen.getByLabelText(/4326/)).toBeInTheDocument()
     expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument()
+  })
+
+  it('should handle select card when clicking on the label', async () => {
+    renderWithTheme(<PaymentOptions cards={cards} handlePayment={jest.fn} />)
+
+    userEvent.click(screen.getByLabelText(/4325/))
+    await waitFor(() => {
+      expect(screen.getByRole('radio', { name: /4325/ })).toBeChecked()
+    })
+  })
+
+  it('should not call handlePayment when button is disabled', () => {
+    const handlePayment = jest.fn()
+    renderWithTheme(
+      <PaymentOptions cards={cards} handlePayment={handlePayment} />
+    )
+
+    userEvent.click(screen.getByRole('button', { name: /buy now/i }))
+    expect(handlePayment).not.toHaveBeenCalled()
+  })
+
+  it('should call handlePayment when credit card is selected', async () => {
+    const handlePayment = jest.fn()
+    renderWithTheme(
+      <PaymentOptions cards={cards} handlePayment={handlePayment} />
+    )
+
+    userEvent.click(screen.getByLabelText(/4325/))
+    userEvent.click(screen.getByRole('button', { name: /buy now/i }))
+
+    await waitFor(() => {
+      expect(handlePayment).toHaveBeenCalled()
+    })
   })
 })
