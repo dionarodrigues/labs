6bc1772e54f28842655547c4ac458666146d55d7
diogorodrigues <diogorodrigues.dev@gmail.com>
2021-03-03 21:26:42 +0000

    improve TextField

--- ecommerce-nextjs-strapi/client/src/components/TextField/styles.ts
+++ ecommerce-nextjs-strapi/client/src/components/TextField/styles.ts
@@ -15,6 +15,7 @@ export const InputWrapper = styled.div`
     padding: 0 ${theme.spacings.xsmall};
     border: 0.2rem solid;
     border-color: ${theme.colors.lightGray};
+
     &:focus-within {
       box-shadow: 0 0 0.5rem ${theme.colors.primary};
     }
@@ -48,8 +49,10 @@ export const Icon = styled.div<IconPositionProps>`
     display: flex;
     color: ${theme.colors.gray};
     order: ${iconPosition === 'right' ? 1 : 0};
+
     & > svg {
       width: 2.2rem;
+      height: 100%;
     }
   `}
 `
@@ -66,6 +69,7 @@ const wrapperModifiers = {
     ${InputWrapper} {
       border-color: ${theme.colors.red};
     }
+
     ${Icon},
     ${Label} {
       color: ${theme.colors.red};
@@ -77,6 +81,7 @@ const wrapperModifiers = {
     ${Icon} {
       cursor: not-allowed;
       color: ${theme.colors.gray};
+
       &::placeholder {
         color: currentColor;
       }
