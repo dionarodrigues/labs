ee6a8091eda8ddef7d6dddd114176600a566e9b3
diogorodrigues <diogorodrigues.dev@gmail.com>
2021-05-09 15:48:49 +0100

    fix price info

--- ecommerce-nextjs-strapi/client/src/components/GameCard/index.tsx
+++ ecommerce-nextjs-strapi/client/src/components/GameCard/index.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 
 import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import Button from 'components/Button'
+import formatPrice from 'utils/format-price'
 import * as S from './styles'
 
 export type GameCardProps = {
@@ -14,8 +15,8 @@ export type GameCardProps = {
   title: string
   developer: string
   img: string
-  price: string
-  promotionalPrice?: string
+  price: number
+  promotionalPrice?: number
   favorite?: boolean
   ribbon?: React.ReactNode
   ribbonColor?: RibbonColors
@@ -62,8 +63,10 @@ const GameCard = ({
         )}
       </S.FavButton>
       <S.BuyBox>
-        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
-        <S.Price>{promotionalPrice || price}</S.Price>
+        {!!promotionalPrice && (
+          <S.Price isPromotional>{formatPrice(price)}</S.Price>
+        )}
+        <S.Price>{formatPrice(promotionalPrice || price)}</S.Price>
         <Button icon={<AddShoppingCart />} size="small" />
       </S.BuyBox>
     </S.Content>
