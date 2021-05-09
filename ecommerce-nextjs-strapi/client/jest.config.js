9693a6fdd758b1c4d9a2ccf8b8cbd3a8ffdf88e7
diogorodrigues <diogorodrigues.dev@gmail.com>
2021-05-09 15:59:19 +0100

    remove some files of jest testing

--- ecommerce-nextjs-strapi/client/jest.config.js
+++ ecommerce-nextjs-strapi/client/jest.config.js
@@ -2,7 +2,17 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/utils/apollo.ts',
+    '!src/utils/apolloCache.ts',
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
