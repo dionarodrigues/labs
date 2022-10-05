928aa8ed2e1408613cefa0082f5ed320d0ab2bbd
Diogo Rodrigues <diogorodrigues.dev@gmail.com>
2022-10-05 21:14:23 +0100

    Add Tip Calculator project

---
+++ tip-calculator/README.md
@@ -0,0 +1,16 @@
+# 🌞🌚🌈 Tip Calculator with Vanilla JavaScript
+
+[Demo on Codepen](https://codepen.io/diogorodrigues/pen/LYGmwZq)
+
+<img src="_screenshot/tip-calculator.jpg" alt="Tip Calculator Screenshot" style="width:100%"/>
+
+---
+
+**A simple calculator using a few lines of Javascript on top of [Tailwind CSS](https://tailwindui.com/)**
+
+This JS application cover the following functionalities:
+
+- total of the bill
+- total of the tip in percentage
+- quantity of people
+- total per person
---
+++ tip-calculator/scripts.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const $billTotalInput = document.querySelector('[name="bill"]');
+const $tipTotalInput = document.querySelector('[name="tip"]');
+const $numberOfPeopleEl = document.querySelector('.peopleTotal');
+const $result = document.querySelector('.result');
+
+const $peopleDecreaseBtn = document.querySelector('.peopleDecreaseBtn');
+const $peopleIncreaseBtn = document.querySelector('.peopleIncreaseBtn');
+
+let peopleTotal = Number($numberOfPeopleEl.textContent);
+
+const handleTotalPerPerson = (tipTotal, billTotal, peopleTotal) => {
+	return ((100 * tipTotal) / billTotal + billTotal) / peopleTotal;
+};
+
+const updateResultUI = result => ($result.innerHTML = result);
+
+const calculateBill = () => {
+	const billTotal = Number($billTotalInput.value);
+	const tipTotal = Number($tipTotalInput.value);
+
+	if (billTotal && tipTotal && peopleTotal) {
+		const total = handleTotalPerPerson(tipTotal, billTotal, peopleTotal);
+		updateResultUI(total.toFixed(2));
+	}
+};
+
+const increasePeople = () => {
+	peopleTotal += 1;
+	$numberOfPeopleEl.textContent = peopleTotal;
+	calculateBill();
+};
+
+const decreasePeople = () => {
+	if (peopleTotal > 1) {
+		peopleTotal -= 1;
+		$numberOfPeopleEl.textContent = peopleTotal;
+		calculateBill();
+	}
+};
+
+const handleInputChange = () => {
+	calculateBill();
+};
+
+$peopleIncreaseBtn.addEventListener('click', increasePeople);
+$peopleDecreaseBtn.addEventListener('click', decreasePeople);
+$billTotalInput.addEventListener('keyup', handleInputChange);
+$tipTotalInput.addEventListener('keyup', handleInputChange);
